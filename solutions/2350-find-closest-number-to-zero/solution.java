b991a300ab0743996267a6db6d84991fc041d68b
Swapnadeep-9182 <141207831+Swapnadeep-9182@users.noreply.github.com>
2026-03-03 16:39:22 +0000

    Sync LeetCode submission - Find Closest Number to Zero - Runtime - 3 ms (55.32%), Memory - 46.8 MB (76.10%)

---
+++ solutions/2350-find-closest-number-to-zero/solution.java
@@ -0,0 +1,25 @@
+
+class Solution {
+    public int findClosestNumber(int[] nums) {
+        int closest = nums[0];
+        for(int x : nums){
+            if (Math.abs(x) < Math.abs(closest)){
+                closest = x;
+            }
+        }
+        if (closest < 0 && contains(nums, Math.abs(closest))) {
+            return Math.abs(closest);
+        }
+        else{
+            return closest;
+        }
+    }
+    private boolean contains(int[] nums, int value){
+        for(int i : nums){
+            if(i == value){
+                return true;
+            }
+        }
+        return false;
+    }
+}
