28473a8c02bdb03a659a49ab8b89d13f5f44b312
Swapnadeep-9182 <141207831+Swapnadeep-9182@users.noreply.github.com>
2026-04-13 06:25:45 +0000

    Sync LeetCode submission - Number of Islands - Runtime - 3 ms (86.79%), Memory - 52.4 MB (46.65%)

---
+++ solutions/0200-number-of-islands/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int i, int j) {
+        // 1. Check bounds and if the current cell is water ('0')
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+
+        // 2. Mark the current cell as visited by setting it to '0'
+        grid[i][j] = '0';
+
+        // 3. Visit all neighbors
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
+
