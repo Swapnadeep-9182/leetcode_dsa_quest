c4c57ff48b1dea230e1b1f079aaebba6686d3212
Swapnadeep-9182 <141207831+Swapnadeep-9182@users.noreply.github.com>
2026-05-18 14:14:43 +0000

    Sync LeetCode submission - Implement Queue using Stacks - Runtime - 0 ms (100.00%), Memory - 19.4 MB (49.14%)

--- solutions/0232-implement-queue-using-stacks/solution.py
+++ solutions/0232-implement-queue-using-stacks/solution.py
@@ -1,31 +1,35 @@
 class MyQueue:
 
     def __init__(self):
-        self.s1 = []
-        self.s2 = []
+        # Stack 1: Our Input Room (where new people join the line)
+        self.stack1 = []
+        # Stack 2: Our Output Room (where we serve the oldest people)
+        self.stack2 = []
 
     def push(self, x: int) -> None:
-        self.s1.append(x)
+        # New guy joins? Just throw him in Stack 1. Super fast! O(1)
+        self.stack1.append(x)
 
     def pop(self) -> int:
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-        return self.s2.pop()
+        # We need to serve someone. Is Stack 2 empty?
+        if not self.stack2:
+            # If yes, bring everyone from Stack 1 into Stack 2 (reversing their order)
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        
+        # Serve the guy at the top of Stack 2
+        return self.stack2.pop()
 
     def peek(self) -> int:
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-        return self.s2[-1]
+        # Who is next in line? 
+        # Same logic as pop, we need to make sure the oldest guy is ready in Stack 2
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        
+        # Just look at him, don't remove him! In Python, [-1] gets the last element (top of the stack)
+        return self.stack2[-1]
 
     def empty(self) -> bool:
-        return max(len(self.s1), len(self.s2)) == 0
-
-
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
+        # The momo stall is only empty if BOTH rooms are completely empty
+        return not self.stack1 and not self.stack2
