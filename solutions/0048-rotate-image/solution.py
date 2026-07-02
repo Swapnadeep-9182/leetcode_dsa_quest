b1c959f020bd0f789db6da4b5a46270afd60b0f2
Swapnadeep-9182 <141207831+Swapnadeep-9182@users.noreply.github.com>
2026-07-02 16:26:44 +0000

    Sync LeetCode submission - Rotate Image - Runtime - 0 ms (100.00%), Memory - 31.5 MB (6.09%)

---
+++ solutions/0048-rotate-image/solution.py
@@ -0,0 +1,7 @@
+import numpy as np
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        matrix[:] = np.rot90(matrix,-1).tolist()
