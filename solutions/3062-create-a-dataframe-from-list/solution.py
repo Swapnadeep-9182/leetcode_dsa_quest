aa90e84cbf36bd7e8872196fae086fdbaf774f3d
Swapnadeep-9182 <141207831+Swapnadeep-9182@users.noreply.github.com>
2026-07-06 15:40:08 +0000

    Sync LeetCode submission - Create a DataFrame from List - Runtime - 255 ms (57.41%), Memory - 66 MB (36.35%)

--- solutions/3062-create-a-dataframe-from-list/solution.py
+++ solutions/3062-create-a-dataframe-from-list/solution.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    column_name = ["student_id","age"]
-    result = pd.DataFrame(student_data, columns=column_name)
-    return result
+   column_names = ["student_id","age"]
+   df = pd.DataFrame(student_data, columns = column_names)
+   return df
